63290fc9927ebcd13129c6f7076bbdeaf1415d5e
Mosobe Lulu <mosobelulu@gmail.com>
2024-04-03 21:48:34 +0200

    WIP

---
+++ my-landing-page/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <h1>coffee shop</h1>
+  )
+}
+
+export default App
+// https://cdn.dribbble.com/users/2530765/screenshots/6707144/coffee_shop_4x.jpg?compress=1&resize=1600x1200
