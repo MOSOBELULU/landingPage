ebe58625a9bc0899ebfa2037559e828320a96f1c
Mosobe Lulu <mosobelulu@gmail.com>
2024-04-22 21:54:15 +0200

    wip

--- my-landing-page/src/components/Home.jsx
+++ my-landing-page/src/components/Home.jsx
@@ -4,6 +4,7 @@ import classes from './Home.module.css'
 function Home() {
     return (
         <div>
+            <img src='images/cuppooo.png' alt='img1' className={classes.img1} />
             <h1>Coffee shop</h1>
            
         </div>
